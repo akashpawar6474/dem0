5b46d2666ae9c2f475d73854556992f13f07db05
akashpawar6474 <59306116+akashpawar6474@users.noreply.github.com>
2019-12-28 21:51:44 +0530

    Update calculatepost.js

--- exp/calculatepost.js
+++ exp/calculatepost.js
@@ -10,6 +10,30 @@ app.get("/",function(req,resp){
     console.log("file send to response");
 });
 
+app.get("/anna",function(req,resp){
+    console.log("happy new year anna");
+});
+
+app.get("/amitguddu",function(req,resp){
+    console.log("happy new year amit ani guddi");
+});
+
+app.get("/mama",function(req,resp){
+    console.log("happy new year mama");
+});
+
+app.get("/aau",function(req,resp){
+    console.log("happy new year aau");
+});
+
+app.get("/aaipappa",function(req,resp){
+    console.log("happy new year aai ani pappa");
+});
+
+app.get("/atya",function(req,resp){
+    console.log("happy new year atya");
+});
+
 app.post("/calculate",function(req,resp){
     result=mymodule.addition(req.body.num1,req.body.num2);
      resp.send("Addition: "+result);
